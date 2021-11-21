aabbfd6475a145c7f02efeee90cb4635ce45391b
Fernanda_TC <ftorres@estudiantes.crackthecode.la>
2021-11-21 16:18:38 -0500

    cambio_1

---
+++ asset/js/index.js
@@ -0,0 +1,61 @@
+// Swal.fire({
+// 	// title:"hola",
+// 	// text:
+// 	html:' <img src="https://gifsanimados.de/img-gifsanimados.de/a/anime/abrazo-virtual.gif" alt="cargando">'
+// 	// icon:
+// 	// confirmButtonText:
+// 	// footer:
+// 	// width:"280px",
+// 	// padding:"20px",
+// 	// background:
+// 	// grow:
+// 	// backdrop:
+// 	// timer:
+// 	// timerProgressBar:
+// 	// toast:
+// 	// position:
+// 	// allowOutsideClick:
+// 	// allowEscapeKey:
+// 	// allowEnterKey:
+// 	// stopKeydownPropagation:
+
+// 	// input:
+// 	// inputPlaceholder:
+// 	// inputValue:
+// 	// inputOptions:
+	
+// 	//  customClass:
+// 	// 	container:
+// 	// 	popup:
+// 	// 	header:
+// 	// 	title:
+// 	// 	closeButton:
+// 	// 	icon:
+// 	// 	image:
+// 	// 	content:
+// 	// 	input:
+// 	// 	actions:
+// 	// 	confirmButton:
+// 	// 	cancelButton:
+// 	// 	footer:	
+
+// 	// showConfirmButton:
+// 	// confirmButtonColor:
+// 	// confirmButtonAriaLabel:
+
+// 	// showCancelButton:
+// 	// cancelButtonText:
+// 	// cancelButtonColor:
+// 	// cancelButtonAriaLabel:
+	
+// 	// buttonsStyling:
+// 	// showCloseButton:
+// 	// closeButtonAriaLabel:
+
+
+// 	// imageUrl:
+// 	// imageWidth:
+// 	// imageHeight:
+// 	// imageAlt:
+// });
+// buscar un gid frl internet
