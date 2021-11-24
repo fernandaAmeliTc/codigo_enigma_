8c7e75e814e6f5bc2354a4f0e370b7a0ba20c32a
fernanda_tc <ftorres@estudiantes.crackthecode.la>
2021-11-24 11:01:22 -0500

    cambio 6

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,54 +1,58 @@
+// alerta inicial
 Swal.fire({
     title:"¡¡¡Bienvenidos a el codigo enigma!!!",
     text:"Lo que coloques en 'el campo de texto' sera encriptado al precionar cifrar y si quieres que vuelva ha estar normal presionas desifrar",
-// 	// footer:
-// 	// width:"280px",
-// 	// padding:"20px",
-// 	// background:
-// 	// grow:
-// 	// backdrop:
 	timer:"22000",
     timerProgressBar:true,
 
 
     });
-// buscar un gid frl internet
+// contador de palabras contador_palabras !Evento input!
+document.getElementById("textarea_msj").addEventListener("input",()=>{
+    var textarea=document.getElementById("textarea_msj");
+    var contador=document.getElementById("contador_palabras");
+    contador.innerHTML=textarea.value.length;
 
-document.getElementById("btn-cambiar").addEventListener("click",function(){
+})
+// button para cifrar
+
+document.getElementById("btn-cifrar").addEventListener("click",function(){
+    var input = parseInt(document.getElementById("msj").value);
+
     var mensaje = document.getElementById("textarea_msj").value;
     var codigosNew = [];
     var nuevoMsj="";
     for(let i=0;i<mensaje.length;i++){
-        var codMsj = mensaje[i].charCodeAt();
-        var codNew = codMsj + 1;
-        codigosNew.push(codNew);
+        var c =mensaje[i].toLowerCase();
+        var codMsj = c.charCodeAt();
+        var codNew = codMsj + input;
+        if( codNew > 122){
+            var d = codNew-122;
+            codNew = 96 + d
+        }
+
         nuevoMsj += String.fromCharCode(codNew);
-    // if(codNew < 23){
-    //     codMsj + 20;
-    // }
 
     }
 
     document.getElementById("new_msj").innerHTML = nuevoMsj
 
-    
-})
 
-// contador de palabras--
-// function getId (id){
-//     return getElementById(id).value;
-// }
-// function innerHTML (id , resultado){
-//     return getElementById(id).innerHTML=resultado;
-// }
-// function contadorDeCaracteres(){
-//     setInterval(function(){
-//         var c =getId("textarea_msj");
-//         if(c.length>300){
-//             innerHTML("contador_palabras","solo se permite 300 caracteres")
-//         } else{
-//             innerHTML("contador_palabras",c.length)
-//         } 
-//     },0000);
-    
-// }
+})
+// button para decifrar
+document.getElementById("btn-decifrar").addEventListener("click",function(){
+    var mensaje = document.getElementById("textarea_msj").value;
+    var codigosNew = [];
+    var nuevoMsj="";
+    for(let i=0;i<mensaje.length;i++){
+        var c =mensaje[i].toLowerCase();
+        var codMsj = c.charCodeAt();
+        var codNew = codMsj + 0;
+
+        nuevoMsj += String.fromCharCode(codNew);
+    }
+
+    document.getElementById("new_msj").innerHTML = nuevoMsj
+
+
+})
